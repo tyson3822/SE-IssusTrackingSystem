7fe0ff294a15d353b4d18f4e7a95c612dc8da643
cg100111 <cg100111@gmail.com>
2016-11-22 23:06:32 +0800

    修改login畫面,首頁要改成從login開始

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,109 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <p class="navbar-brand">Welcome!</p>
+                </dvi>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <p class="text-align:center"><strong><h1>Issue Tracking System</h1></strong></p>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <label class="control-label" style="font-size:18px;color:#636b6f">Sign In</label>
                         </div>
+                        <div class="panel-body">
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                                {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">        
+                                    <label for="email" class="col-md-4 control-label">User Name</label>
+                                    <div class="col-md-offset-1">
+                                        <input id="email" type="email" class="form-control" style="width:90%" name="email" value="{{ old('email') }}" required autofocus>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    <label for="password" class="col-md-4 control-label" style="text-align:center">Password</label>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
+                                    <div class="col-md-offset-1">
+                                        <input id="password" type="password" class="form-control" style="width:90%" name="password" required>
+                                    </div>
+                                </div>
+
+                                @if ($errors->has('email') || $errors->has('password'))
+                                    <div class="form-group">
+                                        <div class="col-md-offset-1">
+                                            <p style="font-size:18px;color:#FF0000">Wrong account or password!<br>Please try another</control-label>
+                                        </div>
+                                    </div>
+                                @endif
+
+                                <div class="form-group">
+                                    <div class="col-md-offset-1">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="remember"> Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group" style="border-bottom-width:1px;border-bottom-style:solid;border-color:#d3e0e9">
+                                    <div class="col-md-offset-1">
+                                        <button type="submit" class="btn btn-primary">Login</button>
+
+                                        <div class=row>
+                                            <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-offset-1 control-label">New User</label>
+
+                                    <div class="row col-md-offset-1">
+                                        <button type="submit" class="btn btn-primary">Sign Up</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+    <!-- Scripts -->
+    <script src="/js/app.js"></script>
+</body>
+</html>
