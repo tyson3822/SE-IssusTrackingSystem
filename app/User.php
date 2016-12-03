24fc50fe591a98f45460352ee83bdec75fdaff97
MOONYAN <1787hz@gmail.com>
2016-12-03 12:29:03 +0800

    建立 user 與 project 關聯

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the projects that user join.
+     */
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class,'project_user_relations')->withPivot('user_auth');
+    }
 }
