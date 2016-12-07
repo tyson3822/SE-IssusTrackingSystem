3e1b540cab80e6fa34935392ecb5c4671b3ae797
cg100111 <cg100111@gmail.com>
2016-12-07 14:05:21 +0800

    新增2個路由,一個是接收使用者變更資訊用,一個是進入特定project用

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 
 Route::group(['middleware' => ['web']], function () {
 
+
 	Route::get('/','HomeController@index');
 
 	Auth::routes();
@@ -21,22 +22,19 @@ Route::group(['middleware' => ['web']], function () {
 
 	//導向Setting頁面,傳入使用者的資料(user_name,email,password,access)
 	Route::get('/Setting',function(){
+		$user = ['name'=>'ABC','email'=>'a@b.com','password'=>'123456','access'=>'admin'];
+
 		return view('Setting')
-			->with('user_name','ABC')
-			->with('email','a@b.com')
-			->with('password','123456')
-			->with('access','admin');
+			->with('user',$user);
 	});
 
 	//接收使用者更新的自己的資料(user_name,password,email)(null代表沒有更改,email用來指向使用者)
 	//導向Setting頁面,傳入使用者的資料(user_name,email,password,access)
-	Route::put('/Setting/{email}/{user_name?}/{password?}',function ($user_name = null,$email,$password = null){
+	Route::put('/Setting',function (){
+		$user = ['name'=>'abc','email'=>'a@b.com','password'=>'123','access'=>'admin'];
 
 		return view('Setting')
-			->with('user_name','abc')
-			->with('email','a@b.com')
-			->with('password','123')
-			->with('access','admin');
+			->with('user',$user);
 	});
 
 	//導向Access_Manage頁面,傳入目前使用者的名稱,以及所有使用者的名稱和權限(陣列)
@@ -50,7 +48,16 @@ Route::group(['middleware' => ['web']], function () {
 			->with('user_name','abc');
 	});
 
+	//接收使用者權限變更資訊
+	//導向Access_Manage,傳入目前使用者的名稱,以及所有使用者的名稱和權限(陣列)(更新)
+	Route::put('/Access_Manage',function(){});
+
 	//導向Project List頁面
+
 	Route::get('/projectlist','ProjectController@index');
+
+	//進入點擊的project裡點,導向Issue List頁面
+	Route::get('/IssueList/{project}',function(){});
+
 
 });
