7cbdbe799c2958710bdbd3b69a1af23313f2b3dc
cg100111 <cg100111@gmail.com>
2016-12-18 14:21:57 +0800

    暫時變更判斷是否是admin的條件, 之後要改回來

--- resources/views/layouts/AccountList_navbar.blade.php
+++ resources/views/layouts/AccountList_navbar.blade.php
@@ -1,5 +1,5 @@
 <?php 
-    if($user['access'] == 'admin'){
+    if(/*$user->role == 'admin' or*/ $user['access'] == 'admin'){
  ?>
         <li><a href="{{url('/Access_Manage')}}">AccountList</a></li>
 <?php
