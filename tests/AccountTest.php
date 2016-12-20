1060bbe663e65e2caa43fafed7847859c8de1b3d
Tyson382 <tyson13032303@gmail.com>
2016-12-20 13:29:14 +0800

    新增修改帳號資料testcase

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -29,7 +29,6 @@ class AccountTest extends TestCase
     		 ->seeInDatabase('users', [
     		 	'name' => 'test01', 
     		 	'email' => 'test01@gmail.com']);
-
     }
 
     //帳號登入
@@ -42,5 +41,46 @@ class AccountTest extends TestCase
              ->type('testaccount', 'password')
              ->press('Login')
              ->see('You are logged in!');
+    }
+
+    //修改帳號個人資料
+    public function testEditPersonalData()
+    {
+        $this->visit('/')
+             //登入
+             ->click('Login')
+             ->seePageIs('/login')
+             ->type('test@gmail.com', 'email')
+             ->type('testaccount', 'password')
+             ->press('Login')
+             ->see('You are logged in!') //登入後的頁面待改
+
+             //個人資料設定
+             ->press('Setting')
+             ->seePageIs('/Setting')
+             ->see('test')
+             ->see('test@gmail.com')
+             ->see('testaccount')
+             ->seeInDatabase('user', [
+                'name' => 'test',
+                'email' => 'test@gmail.com'])
+             ->type('testEdited', 'name')
+             ->type('tessaccountedited', 'password')
+             ->press('Save')
+
+             //確認資料庫資料正確(沒辦法驗證密碼)
+             ->seeInDatabase('user', [
+                'name' => 'testEdited',
+                'email' => 'test@gmail.com'])
+
+             //登出再登入驗證密碼
+             ->press('Logout')
+             ->press('Login')
+             ->seePageIs('/login')
+             ->type('testEdited', 'name')
+             ->type('tessaccountedited', 'password')
+             ->press('Login')
+             ->see('You are logged in!')  //登入後的頁面待改
+             
     }
 }
