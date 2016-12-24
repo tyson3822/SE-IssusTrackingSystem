bc8d36c8f03684174cb83186e8044c66f9e51a77
cg100111 <cg100111@gmail.com>
2016-12-25 00:33:21 +0800

    修改進入專案時,會進入錯誤的bug 原因是因為是用呼叫form的id的方式,但因為foreach迴圈所以畫面上有好幾個相同id的form 所以都呼叫到第一個 可以正確進入專案

--- resources/views/ProjectList.blade.php
+++ resources/views/ProjectList.blade.php
@@ -67,24 +67,24 @@
             <div class="col-md-4">
                 <div class="panel panel-info" style="padding-left: 0px;padding-right: 0px;">
                     <div class="panel-heading">
-                        <div class="panel-title">
-                            {{$project->subject}}
-                            <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-sm" role="button" onclick="event.preventDefault();document.getElementById('GotoProject').submit();">Go</a>
-                                {{$project->id}}
-                                <form id="GotoProject" method="GET" style="display: none;" action="{{ url('/project/'.$project->id) }}">
-                                    {{ csrf_field() }}
-                                </form>
-                                @if($project->pivot['user_auth'] == 'manager')
-                                    <!--<button type="button" class="close" data-toggle="modal" data-target="#CloseProjectModal" data-project_name="{{$project->subject}}">&times;</button>-->
-
-                                    <button type="submit" class="close" onclick="event.preventDefault();document.getElementById('CloseProject').submit();">&times;</button>
-                                    <form id="CloseProject" action="{{ url('Close_Project',$project->id) }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                        {{ method_field('PUT') }}
-                                    </form>
-                                @endif
+                        <div class="panel-title  clearfix">
+                            <div class="pull-left">
+                                {{$project->subject}}
                             </div>
+                            @if($project->pivot['user_auth'] == 'manager')
+                                <!--<button type="button" class="close" data-toggle="modal" data-target="#CloseProjectModal" data-project_name="{{$project->subject}}">&times;</button>-->
+
+                                <form method="POST" action="{{ url('Close_Project',$project->id) }}">
+                                    {{ csrf_field() }}
+                                    {{ method_field('PUT') }}
+                                    <button type="submit" class="close" style="margin-left: 5px;">&times;</button>
+                                </form>
+                            @endif
+
+                            <form method="GET" action="{{ url('/project',['project_id' =>$project->id]) }}">
+                                {{ csrf_field() }}
+                                <button type="submit" class="btn btn-default btn-sm pull-right" style="padding: 4px;">Go</button>
+                            </form>
                         </div>
                     </div>
                     <div class="panel-body">
