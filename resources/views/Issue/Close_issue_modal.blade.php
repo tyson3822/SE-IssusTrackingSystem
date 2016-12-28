b2536e96529856a8d859f037aa04e8d5bb673047
MOONYAN <1787hz@gmail.com>
2016-12-28 18:18:06 +0800

    fixbug fix action at 'Close_issue_blade'

--- resources/views/Issue/Close_issue_modal.blade.php
+++ resources/views/Issue/Close_issue_modal.blade.php
@@ -6,7 +6,7 @@
                 <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
                 <h4 class="modal-title" id="myModalLabel">Close Issue</h4>
             </div>
-            <form class="form-horizontal" role="form" method="POST" action="/Create_Issue">
+            <form class="form-horizontal" role="form" method="POST" action="/Delete_issue">
                 {{ csrf_field() }}
                 <div class="modal-body">
                 </div>
