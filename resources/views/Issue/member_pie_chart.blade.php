a1eac91dfcb42d77227d9f76c02573ae0fdeb02b
cg100111 <cg100111@gmail.com>
2016-12-30 01:09:16 +0800

    針對issue,member產生圖示,目前排版問題尚未解決

---
+++ resources/views/Issue/member_pie_chart.blade.php
@@ -0,0 +1,61 @@
+<script type="text/javascript">
+    $(function () {
+        var manager = 0;
+        var developer = 0;
+        var tester = 0;
+        var general = 0;
+        @foreach ($project->users as $member)
+            @if($member->pivot['user_auth'] == 'manager')
+                manager++;
+            @elseif($member->pivot['user_auth'] == 'developer')
+                developer++;
+            @elseif($member->pivot['user_auth'] == 'tester')
+                tester++;
+            @elseif($member->pivot['user_auth'] == 'general')
+                general++;
+            @endif
+        @endforeach
+
+        Highcharts.chart('project_member', {
+            chart: {
+                width: 450,
+                height: 300,
+                type: 'pie'
+            },
+            title: {
+                text: '成員職位統計'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                data: [{
+                    name: 'manager',
+                    y: manager
+                }, {
+                    name: 'developer',
+                    y: developer
+                }, {
+                    name: 'tester',
+                    y: tester
+                },{
+                    name: 'general',
+                    y: general
+                }]
+            }]
+        });
+    });
+</script>
--- resources/views/IssueList.blade.php
+++ resources/views/IssueList.blade.php
@@ -33,13 +33,9 @@
                 <div class="navbar-header">
                     <a class="navbar-brand">ITS</a>
                 </div>
-
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="{{route('project_list')}}">專案</a>
-                </div>
-
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav">
+                        <a class="navbar-brand" href="{{route('project_list')}}">專案</a>
                         <li class="active"><a href="{{ route('issue_list',['project_id' => $project->id]) }}">議題</a></li>
                         @include('layouts.AccountList_navbar')
                     </ul>
@@ -77,14 +73,25 @@
                 <div class="table-responsive">
                     <table class="table table-hover">
 
-                        <tr><th>#</th><th>標題</th><th>嚴重程度</th><th>狀態</th><th>建立時間</th><th></th><th></th></tr>
+                        <tr>
+                            <th>#</th>
+                            <th>標題</th>
+                            <th>嚴重程度</th>
+                            <th>狀態</th>
+                            <th>建立時間</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
                         <?php
                             $index = 0;
                         ?>
-                        @foreach ($project->issues as $issue)        
-                                
-                        <tr ><td>{{$issue->id}}</td><td>{{$issue->title}}</td><td>{{$issue->priority}}</td><td>{{$issue->state}}</td><td>{{$issue->created_at}}</td>
-
+                        @foreach ($project->issues as $issue)           
+                        <tr>
+                            <td>{{$issue->id}}</td>
+                            <td>{{$issue->title}}</td>
+                            <td>{{$issue->priority}}</td>
+                            <td>{{$issue->state}}</td>
+                            <td>{{$issue->created_at}}</td>
                             <td>
                                 <div class="pull-right">
                                     <form id= method="GET" action="{{ route('issue',['project_id' => $project->id,'issue_id' => $issue->id]) }}">
@@ -92,18 +99,16 @@
                                         <button type="submit" class="btn btn-default btn-xs" aria-label="Left Align">
                                             <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
                                         </button>
-                                            <!-- <button type="submit" class="btn btn-default btn-xs">Description</button> -->
                                     </form>             
                                 </div> 
-
                             </td>
                             <td>
                                 @if($project->pivot['user_auth'] == 'manager')
                                     <!--<button type="button" class="close" data-toggle="modal" data-target="#CloseProjectModal" data-project_name="{{$project->subject}}">&times;</button>-->
 
                                 <form method="POST" action="{{ route('Delete_issue',['project_id' => $project->id,'issue_id' => $issue->id]) }}">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
                                     <button type="submit" class="close">&times;</button>
                                 </form>
                                 @endif                      
@@ -111,16 +116,38 @@
                         </tr>      
                         @endforeach
                     </table>
+                </div>   
+            </div>
+            <div class="row" style="padding: 0px">
+                <div class="row">
+                    <br>
+                    <button type="button" class="btn btn-success col-md-offset-5 col-md-1">圓餅圖</button>
+                    <button type="button" class="btn btn-info col-md-1" style="margin-left: 5px">長條圖</button>
                 </div>
                 
+                <div class="pie_chart">
+                    <div id="issue_state" style="width: 450px; height: 300px;"></div>
+                    <div id="issue_priority" style="width: 450px; height: 300px;"></div>
+                    <div id="project_member" style="width: 450px; height: 300px;"></div>
+                </div>
+                <div class="column_chart">
+                    
+                </div>
             </div>
-        
         </div>
     </div>
 
+    <!-- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <!-- Highcharts -->
+    <script src="http://code.highcharts.com/highcharts.js"></script>
 
     <!-- Referencing Bootstrap JS that is hosted locally -->
     <script src="/js/bootstrap.min.js"></script>
+
+    @include('Issue.state_pie_chart')
+    @include('Issue.priority_pie_chart')
+    @include('Issue.member_pie_chart')
 </body>
 </html>
